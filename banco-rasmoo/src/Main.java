4ab156e0705b5d7ebcfefaf07e31c5badd083079
henriquepst <joaohenrique2@hotmail.com>
2025-09-27 21:51:40 -0300

    valores default de atributos

--- banco-rasmoo/src/Main.java
+++ banco-rasmoo/src/Main.java
@@ -2,15 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         Conta primeiraConta = new Conta();
-        primeiraConta.saldo = 200;
-        primeiraConta.agencia = 200;
-        primeiraConta.numero = 100;
-        primeiraConta.titular = "Pedro Henrique";
+        primeiraConta.saldo = 1002;
 
-        System.out.println("o titular da primeira conta é " + primeiraConta.titular);
-        System.out.println("Agencia : " + primeiraConta.agencia);
-        System.out.println("Saldo : " + primeiraConta.saldo);
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 2000;
 
+        System.out.println(primeiraConta.saldo);
+        System.out.println(segundaConta.saldo);
 
+        System.out.println(primeiraConta.agencia);
+        System.out.println(primeiraConta.numero);
+
+        /* Quando você declara atributos de uma classe, eles recebem valores padrão automaticamente:
+
+        Tipo de dado	Valor padrão
+        int, byte =	0
+        double, float = 0.0
+        boolean	= false
+        char = \u0000
+        Objetos (String, etc) =	null  */
     }
 }
