846a17240138d4b0f6c100100a59dfea06201a75
henriquepst <joaohenrique2@hotmail.com>
2025-09-28 20:37:33 -0300

    o famoso null e o primeiro contato  com nullpointerexception

---
+++ banco-rasmoo/src/TesteNull.java
@@ -0,0 +1,21 @@
+public class TesteNull {
+
+
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+
+
+        System.out.println(conta.agencia);
+        System.out.println(conta.numero);
+        System.out.println(conta.saldo);
+
+        System.out.println(conta.titular);
+
+        conta.titular = new Cliente();
+
+        conta.titular.nome = "Pedro";
+
+        System.out.println("o nome do titular" + conta.titular.nome);
+
+    }
+}
